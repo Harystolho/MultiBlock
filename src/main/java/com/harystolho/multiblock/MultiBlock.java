5448a81cf908369115b44dd8b897c50043f9d543
Harystolho <haristolhogamer@gmail.com>
2019-07-04 13:17:30 -0300

    Add main class

--- src/main/java/com/harystolho/multiblock/MultiBlock.java
+++ src/main/java/com/harystolho/multiblock/MultiBlock.java
@@ -1,9 +1,23 @@
 package com.harystolho.multiblock;
 
-public class MultiBlock 
-{
-    public static void main( String[] args )
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = MultiBlock.MULTIBLOCK_ID)
+public class MultiBlock {
+
+	public static final String MULTIBLOCK_ID = "multiblock";
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+
+	}
+	
+	@EventHandler
+    public void init(FMLInitializationEvent event)
     {
-        
+		
     }
 }
